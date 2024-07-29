23b2d6dfce330b6a6b0c4c896f5ec173ea26d613
vikkoooo <vikkolundberg@gmail.com>
2024-07-29 15:45:22 +0200

    wrap correctly

--- src/Song.tsx
+++ src/Song.tsx
@@ -4,8 +4,11 @@ export function Song(): ReactElement {
 	return (
 		<div className="song">
 			<p>image of song here</p>
-			<p className="artist-name">John Lennon</p>
-			<p className="song-name">Save the room</p>
+			<div className="song-info">
+				<p className="artist-name">John Lennon</p>
+				<p className="song-name">Save the room</p>
+			</div>
+			<span className="material-symbols-outlined">play_circle</span>
 		</div>
 	);
 }
